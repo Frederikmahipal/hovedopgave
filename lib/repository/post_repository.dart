5bd0b7a2843b8792ecd504612d3a021c132c280c
frederikmahipal <frederikmhv@gmail.com>
2023-05-23 08:41:06 +0200

    post doesnt fully work yet

--- lib/repository/post_repository.dart
+++ lib/repository/post_repository.dart
@@ -7,26 +7,27 @@ class PostRepository {
     if (teamId == null || teamId.isEmpty) {
       return Stream.empty();
     }
-    final postsCollection = _firestore
-        .collection('teams')
-        .doc(teamId)
-        .collection('posts');
-    final snapshots = postsCollection
-        .orderBy('timestamp', descending: true)
-        .snapshots();
+    final postsCollection =
+        _firestore.collection('teams').doc(teamId).collection('posts');
+    final snapshots =
+        postsCollection.orderBy('timestamp', descending: true).snapshots();
     return snapshots;
   }
 
+
+  Future<void> createPost(String teamId, String message) async {
+    final postsCollection = _firestore.collection('teams').doc(teamId);
+
+    await postsCollection.update({'created': FieldValue.serverTimestamp()});
+    await postsCollection.set({'message': message}, SetOptions(merge: true));
+  }
+
   Future<void> addPost(String teamId, String message) async {
-    if (teamId == null || teamId.isEmpty) {
-      return;
-    }
-    final postsCollection = _firestore
-        .collection('teams')
-        .doc(teamId)
-        .collection('posts');
+    final postsCollection =
+        _firestore.collection('teams').doc(teamId).collection('posts');
     final timestamp = DateTime.now().millisecondsSinceEpoch;
     final postData = {'message': message, 'timestamp': timestamp};
     await postsCollection.add(postData);
   }
+
 }
--- lib/view/teams/team_dashboard.dart
+++ lib/view/teams/team_dashboard.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../reusable-widgets/post_form.dart';
